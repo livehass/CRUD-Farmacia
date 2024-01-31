11053909e97705d4b5cc1d566cddab0ed004fda6
livehass <silva.felipe12@hotmail.com>
2024-01-31 20:12:47 -0300

    Added crud categoria

---
+++ src/main/java/com/crudfarmacia/crudfarmacia/model/Categoria.java
@@ -0,0 +1,32 @@
+package com.crudfarmacia.crudfarmacia.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O atributo título é obrigatório!")
+    private String titulo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
