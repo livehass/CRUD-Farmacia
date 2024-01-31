267f68eefc638878b2691a6516f950b0bd6b087a
livehass <silva.felipe12@hotmail.com>
2024-01-31 19:57:42 -0300

    Added inicial files

---
+++ src/main/java/com/crudfarmacia/crudfarmacia/CrudfarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.crudfarmacia.crudfarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudfarmacia/crudfarmacia/CrudfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudfarmacia.crudfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
